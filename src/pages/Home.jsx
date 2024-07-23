f09fdd2a4e8668e7ffa689ce092f5b9002bd20ab
Islom Axmedov <146936087+islomxonahmedov@users.noreply.github.com>
2024-07-23 21:07:28 +0500

    Responsiv navbarlar tayyor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Lorem5000</div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum libero debitis repudiandae sed magnam eligendi ipsum doloremque blanditiis rem quis neque quibusdam id sint, modi asperiores odio beatae ad laudantium atque porro distinctio expedita adipisci itaque? Vel reiciendis, nisi assumenda eos totam molestias minus illum sequi aut, consequatur nesciunt porro delectus laboriosam enim quod itaque esse harum distinctio dolorem dicta! Vero nostrum nobis ut hic veniam ea rerum veritatis inventore? Corrupti sunt id eveniet error, obcaecati debitis asperiores expedita aliquam excepturi modi? Minus saepe ratione non, obcaecati, quod culpa unde, provident amet quia aliquid excepturi totam vel doloremque suscipit quas?</div>
   )
 }
 
